7eeff4b979cca1056abb8da0f424e83a27d8c20c
Husnain <husnainali41940@gmail.com>
2025-04-26 23:44:11 -0700

    Build ui and setup appwrite

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,5 +1,5 @@
 import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
-import { MobileSidebar, NavItems } from "components";
+import { MobileSidebar, NavItems } from "../../../components";
 import { Outlet } from 'react-router'
 
 const AdminLayout = () => {
@@ -7,9 +7,9 @@ const AdminLayout = () => {
     <div className='admin-layout'>
       <MobileSidebar />
       <aside className='w-full max-w-[270px] hidden lg:block'>
-<SidebarComponent width={270} enableGestures={false} >
-<NavItems />
-</SidebarComponent>
+      <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
     </aside>
     <aside className='children'>
 <Outlet />
